f64d6789a415d29744cc7400e5eb74c4836e2a1a
absaaralishah2025-creator <absaaralishah2025@gmail.com>
2026-03-21 11:44:12 +0500

    Update SoulPhantomEntity.java

--- src/main/java/com/ancients/mod/entity/SoulPhantomEntity.java
+++ src/main/java/com/ancients/mod/entity/SoulPhantomEntity.java
@@ -1,22 +1,18 @@
 package com.ancients.mod.entity;
 
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.FlyingMob;
 import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
-/**
- * Soul Phantom - A flying creature that combines Phantom (End) and Blaze (Nether) elements
- * Spawns naturally in the Ancients Dimension
- */
-public class SoulPhantomEntity extends FlyingMob {
+public class SoulPhantomEntity extends Monster {
 
-    public SoulPhantomEntity(EntityType<? extends FlyingMob> type, Level level) {
+    public SoulPhantomEntity(EntityType<? extends Monster> type, Level level) {
         super(type, level);
     }
 
@@ -24,13 +20,13 @@ public class SoulPhantomEntity extends FlyingMob {
     protected void registerGoals() {
         this.goalSelector.addGoal(1, new FloatGoal(this));
         this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0, false));
-        this.goalSelector.addGoal(5, new WaterAvoidingRandomFlyingGoal(this, 1.0));
+        this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.0));
         this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 8.0F));
         this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, true));
     }
 
     public static AttributeSupplier.Builder createAttributes() {
-        return FlyingMob.createMobAttributes()
+        return Monster.createMonsterAttributes()
                 .add(Attributes.MAX_HEALTH, 40.0D)
                 .add(Attributes.MOVEMENT_SPEED, 0.6D)
                 .add(Attributes.ATTACK_DAMAGE, 8.0D)
@@ -44,11 +40,6 @@ public class SoulPhantomEntity extends FlyingMob {
 
     @Override
     public boolean isOnFire() {
-        return true; // Always appears to be on fire (Nether trait)
-    }
-
-    @Override
-    protected boolean shouldDespawnInPeaceful() {
         return true;
     }
 }
